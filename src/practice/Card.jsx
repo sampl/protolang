e5952c150eabd5ddcce9f02e991771c909d36568
Sam Pierce Lolla <sampiercelolla@gmail.com>
2022-07-14 15:37:06 -0400

    anki-style flashcard that saves attempts

--- src/practice/Card.jsx
+++ src/practice/Card.jsx
@@ -1,24 +1,53 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import { supabase } from '@/_util/supabaseClient'
 import Card from '@/styles/Card'
+import { useUser } from '@/_state/user'
+
+const TEST_TYPE = 'self'
 
 export default ({ card, nextCard }) => {
+
+  const { user } = useUser()
+
   const [answerIsShown, setAnswerIsShown] = useState(false)
 
+  const saveAnswer = async correct => {
+    nextCard()
+
+    try {
+      const newData = {
+        type: TEST_TYPE,
+        created_by: user.id,
+        word: card.word.id,
+        correct,
+      }
+
+      let { error } = await supabase.from('attempts').insert([newData])
+
+      if (error) {
+        throw error
+      }
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
   return <Card>
-    <h2>{card?.word.translation_en}</h2>
+    <p>Translate from English to Italian...</p>
+    <h2>{card?.word?.translation_en}</h2>
+    <p>(Think of the answer and hit the button when you're ready)</p>
     {
-    !answerIsShown
-    ?
-    <button onClick={() => setAnswerIsShown(true)}>Reveal</button>
-    :
-    <>
-      <h4>{card?.word.name}</h4>
-      <Link to={`/words/${card.word.id}`}>go to word</Link>
-      <br />
-      <button onClick={nextCard}>Next</button>
-    </>
+      !answerIsShown ?
+      <button onClick={() => setAnswerIsShown(true)}>Show answer</button> :
+      <>
+        <h4>{card?.word.name}</h4>
+        <Link to={`/words/${card.word.id}`}>go to word</Link>
+        <br />
+        <button onClick={() => saveAnswer(true)}>I was right</button>
+        <button onClick={() => saveAnswer(false)}>I was wrong</button>
+      </>
     }
   </Card>
 }
