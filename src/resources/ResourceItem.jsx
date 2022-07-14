02c0a7a487ebbafa3d0d0a8388f7714bbd47ad95
Sam Pierce Lolla <sampiercelolla@gmail.com>
2022-07-14 13:22:36 -0400

    add resource ratings

--- src/resources/ResourceItem.jsx
+++ src/resources/ResourceItem.jsx
@@ -11,14 +11,27 @@ export default function ResourceItem() {
     [id],
   )
 
-  const [{ data, error, fetching }] = useSelect('resources', { filter })
-
+  const [{ data, error, fetching }] = useSelect('resources', {
+    columns: '*, resource_ratings(*)',
+    filter,
+  })
+  
   const resource = data && data[0]
 
   return <Layout>
-    {error && error.message}
-    {fetching && 'loading...'}
-
-    <h1>{resource && resource.url}</h1>
+    {
+      error ? error.message :
+      fetching ? 'loading...' :
+      !resource ? 'no resource found' :
+      <>
+        <h1>{resource.url}</h1>
+        {
+          (!resource.resource_ratings || resource.resource_ratings.length <= 0) ? 'no ratings' :
+          resource.resource_ratings.map(rr => {
+            return <div key={rr.id}>{rr.stars} star(s): {rr.content}</div>
+          })
+        }
+      </>
+    }
   </Layout>
 }
