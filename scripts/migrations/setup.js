2206d196645ca96fce45734bf95d353389084f0e
Sam Pierce Lolla <sampiercelolla@gmail.com>
2022-07-22 16:51:19 -0400

    show real heatmap data

--- scripts/migrations/setup.js
+++ scripts/migrations/setup.js
@@ -4,11 +4,25 @@ require('dotenv').config()
 const connectionString = process.env.ADMIN_POSTGRES_CONNECTION_STRING
 const client = new Client({ connectionString })
 
+// https://supabase.com/blog/2020/11/18/postgresql-views
+const HEATMAP_VIEW = `
+  CREATE OR REPLACE VIEW heatmap_days AS
+    SELECT    date(created_at), created_by, COUNT(*)
+    FROM      attempts
+    WHERE     created_at > CURRENT_DATE - INTERVAL '1 year'
+    GROUP BY  date(created_at), created_by;
+`
+
 const migrate = async () => {
   await client.connect()
   
-  const res = await client.query('SELECT * FROM words')
-  console.log('result:', res.rows[0])
+  try {
+    // const res = await client.query('SELECT * FROM words')
+    // console.log('result:', res.rows[0])
+    await client.query(HEATMAP_VIEW)
+  } catch (error) {
+    console.error(error)
+  }
   
   await client.end()
 }
--- src/views/dashboard/Heatmap.jsx
+++ src/views/dashboard/Heatmap.jsx
@@ -1,35 +1,8 @@
-import { useEffect, useState } from "react"
 import moment from 'moment'
+import { useSelect, useFilter } from 'react-supabase'
 
-// import { supabase } from '@/_util/supabaseClient'
 import { HeatmapCell, HeatmapWrapper } from "@/styles/Heatmap"
-
-const TEST_DAYS = [
-  {
-    date: '2022-01-01',
-    attempts: 0,
-  },
-  {
-    date: '2022-01-02',
-    attempts: 1,
-  },
-  {
-    date: '2022-01-03',
-    attempts: 10,
-  },
-  {
-    date: '2022-01-04',
-    attempts: 75,
-  },
-  {
-    date: '2022-01-06',
-    attempts: 500,
-  },
-  {
-    date: '2022-01-20',
-    attempts: 1000,
-  },
-]
+import { useUser } from "@/_state/user"
 
 const getColor = attempts => {
   return attempts >= 1000 ? 'var(--diablo)' :
@@ -42,49 +15,43 @@ const getColor = attempts => {
 
 export default () => {
 
-  const [error, setError] = useState()
-  const [days, setDays] = useState([])
+  const { user } = useUser()
 
-  useEffect( () => {
-    const getData = async () => {
+  // empty array of days
+  // TODO - offset for day of the week
+  const start = moment().add(-365, 'days')
+  const emptyDays = Array.from(new Array(365))
+    .map( (_, index) => ({
+      date: moment(start).add(index, 'day').format('YYYY-MM-DD'),
+    }))
 
-      // empty array of days
-      // TODO - offset for day of the week
-      const start = moment().add(-365, 'days')
-      const emptyDays = Array.from(new Array(365))
-        .map( (_, index) => ({
-          date: moment(start).add(index, 'day').format('YYYY-MM-DD'),
-        }))
-
-      // const { data: dataDays, error } = await supabase.rpc('get_user_activity_heatmap')
-      const dataDays = TEST_DAYS
-      
-      // merge data with empty array
-      const days = emptyDays.map(ed => {
-        const dataDay = dataDays.find(dd => dd.date === ed.date)
-        return {
-          ...ed,
-          attempts: dataDay?.attempts || 0,
-        }
-      })
-
-      setDays(days)
-      setError(error)
+  // get activity data
+  const filter = useFilter(
+    (query) => query.eq('created_by', user.id),
+    [user.id],
+  )
+  const [{ data: dataDays, error, fetching }] = useSelect('heatmap_days', { filter })
+  
+  // merge data with empty array
+  const days = emptyDays.map(ed => {
+    const dataDay = dataDays?.find(dd => dd.date === ed.date)
+    return {
+      ...ed,
+      attempts: dataDay?.count || 0,
     }
-    getData()
-  }, [])
+  })
 
   return <div>
-    {error && error.message}
-
-    <hr />
-
-    <HeatmapWrapper padding={2}>
-      {days.map(day => {
-        const background = getColor(day.attempts)
-        return <HeatmapCell key={day.date} size={10} background={background} />
-      })}
-    </HeatmapWrapper>
+    {
+      error ? error.message :
+      fetching ? 'loading...' :
+      <HeatmapWrapper padding={2}>
+        {days.map(day => {
+          const background = getColor(day.attempts)
+          return <HeatmapCell key={day.date} size={10} background={background} />
+        })}
+      </HeatmapWrapper>
+    }
   </div>
 
 }
