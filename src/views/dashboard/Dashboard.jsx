f4252299abcb108be16c986e8ebdba39137985b4
Sam Pierce Lolla <sampiercelolla@gmail.com>
2022-07-25 11:05:53 -0400

    add radio station to dashboard

--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -1,20 +1,22 @@
 import { useUser } from "../../_state/user"
-import AttemptsList from '@/views/dashboard/AttemptsList'
 import Heatmap from "./Heatmap"
 import UserScore from "./UserScore"
+import Radio from "./Radio"
+import { TwoColumns } from "@/styles/Layout"
 
 export default () => {
   const { user } = useUser()
   return <>
     <h2>Welcome, {user?.username}</h2>
 
-    <hr />
-
-    <Heatmap />
-    <UserScore />
-    
-    <hr />
-
-    <AttemptsList userId={user.id} />
+    <TwoColumns cols="2fr 1fr">
+      <div>
+        <UserScore />
+        <Heatmap />
+      </div>
+      <div>
+        <Radio />
+      </div>
+    </TwoColumns>
   </>
 }
---
+++ src/views/dashboard/Radio.jsx
@@ -0,0 +1,11 @@
+export default () => {
+  // TODO - not just italian radio, use the language picker
+  return <>
+    <iframe
+      src="https://tunein.com/embed/player/s25211/"
+      style={{width: '100%', height: '100px'}}
+      scrolling="no"
+      frameBorder="no"
+    />
+  </>
+}
