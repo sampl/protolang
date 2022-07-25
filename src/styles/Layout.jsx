b1a3f30c9978d76b001e13b182dec5e7123bb0b0
Sam Pierce Lolla <sampiercelolla@gmail.com>
2022-07-25 12:41:49 -0400

    mobile layout

--- src/styles/Layout.jsx
+++ src/styles/Layout.jsx
@@ -4,4 +4,8 @@ export const TwoColumns = styled.div`
   display: grid;
   grid-template-columns: ${p => p.cols || `1fr 1fr`};
   grid-column-gap: ${p => p.gap ? `${p.gap}rem` : `2rem`};
+
+  @media (max-width: 800px) {
+    grid-template-columns: auto;
+  }
 `
