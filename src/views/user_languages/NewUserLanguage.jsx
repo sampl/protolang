d4cfb1090b69c4a103da8492a430d919bdbb4cc9
Sam Pierce Lolla <sampiercelolla@gmail.com>
2022-08-05 15:57:03 -0400

    more questions when adding a new language

--- src/views/user_languages/NewUserLanguage.jsx
+++ src/views/user_languages/NewUserLanguage.jsx
@@ -14,7 +14,11 @@ export default ({ closeModal }) => {
   const liveLanguages = allLanguages?.filter(l => l.is_live)
   const soonLanguages = allLanguages?.filter(l => !l.is_live)
 
-  const [selectedLanguage, setSelectedLanguage] = useState('lang_it')
+  const [selectedLanguage, setSelectedLanguage] = useState()
+  const [selectedGoal, setSelectedGoal] = useState()
+  const [selectedVisitPlans, setSelectedVisitPlans] = useState()
+  const [selectedVisitDate, setSelectedVisitDate] = useState()
+  const [selectedSkill, setSelectedSkill] = useState()
   const [saving, setSaving] = useState(false)
 
   async function addUserLanguage( event ) {
@@ -24,6 +28,10 @@ export default ({ closeModal }) => {
       const newData = {
         created_by: user.id,
         language: selectedLanguage,
+        goal: selectedGoal,
+        visit_plans: selectedVisitPlans,
+        visit_date: selectedVisitDate,
+        self_reported_skill: selectedSkill,
       }
       let { error } = await supabase.from('user_languages').insert([newData])
       if (error) {
@@ -39,8 +47,6 @@ export default ({ closeModal }) => {
       setSelectedLanguage(undefined)
     }
   }
-
-  console.log(selectedLanguage)
 
   return <form onSubmit={addUserLanguage}>
     <h2>Choose your language</h2>
@@ -82,6 +88,83 @@ export default ({ closeModal }) => {
         </RadioItem>
       })}
 
+    </RadioRoot>
+
+    <label>How serious are you about learning Italian?</label>
+    <RadioRoot
+      value={selectedGoal}
+      onValueChange={value => setSelectedGoal(value)}
+      required
+    >
+      <RadioItem value="playing_around">
+        <RadioIndicator />
+        Just playing around - hobby (your proficiency score goal is 100 words, enough to impress your friends and basic politeness in Italy)
+      </RadioItem>
+      <RadioItem value="some">
+        <RadioIndicator />
+        I want to have some basic conversations - travel coming up, tourists, travelers, (your proficiency score is 1,000 words, enough to have basic conversations in Italian)
+      </RadioItem>
+      <RadioItem value="fluency">
+        <RadioIndicator />
+        I want to be fluent in Italian - immigrants, spouses, employees etc (your proficiency goal is 10,000 words, the same as a fluent COFL B2 speaker)
+      </RadioItem>
+    </RadioRoot>
+
+    <br />
+
+    <label>Do you have plans to visit Italy (or an Italian-speaking part of the world)?</label>
+    <RadioRoot
+      value={selectedVisitPlans}
+      onValueChange={value => setSelectedVisitPlans(value)}
+      required
+    >
+      <RadioItem value="yes_date">
+        <RadioIndicator />
+        Yes!
+      </RadioItem>
+      <RadioItem value="yes_no_date">
+        <RadioIndicator />
+        Yes, but no firm dates yet
+      </RadioItem>
+      <RadioItem value="someday">
+        <RadioIndicator />
+        I'd love to someday!
+      </RadioItem>
+      <RadioItem value="none">
+        <RadioIndicator />
+        Not really
+      </RadioItem>
+    </RadioRoot>
+
+    <br />
+
+    {selectedVisitPlans === 'yes_date' && <>
+      <label>When is your trip?</label>
+      <input
+        type="date"
+        onChange={e => setSelectedVisitDate(e.target.value)}
+      />
+      <br />
+    </>}
+
+    <label>Do you already speak some Italian?</label>
+    <RadioRoot
+      value={selectedSkill}
+      onValueChange={value => setSelectedSkill(value)}
+      required
+    >
+      <RadioItem value="no">
+        <RadioIndicator />
+        Nope!
+      </RadioItem>
+      <RadioItem value="some">
+        <RadioIndicator />
+        Sì, I learned a little bit
+      </RadioItem>
+      <RadioItem value="lots">
+        <RadioIndicator />
+        In realtà parlo un ottimo italiano
+      </RadioItem>
     </RadioRoot>
 
     <br />
