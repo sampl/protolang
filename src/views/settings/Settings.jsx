70e5eb3196987fc1bebbad5e61ef74bbd3f50cce
Sam Pierce Lolla <sampiercelolla@gmail.com>
2022-08-06 14:45:33 -0400

    break out username from settings

--- src/views/settings/Settings.jsx
+++ src/views/settings/Settings.jsx
@@ -1,43 +1,13 @@
-import { useState } from 'react'
-
-import { supabase } from '@/_util/supabaseClient'
 import { useUser } from '@/_state/user'
+import UserName from '@/views/settings/UserName'
 
 export default () => {
   const { user, logout } = useUser()
-  const [username, setUsername] = useState(user?.username)
-  const [loading, setLoading] = useState(false)
-
-  const onSubmit = async event => {
-    event.preventDefault()
-    try {
-      setLoading(true)
-      const user = supabase.auth.user()
-
-      const updates = {
-        id: user.id,
-        username,
-        updated_at: new Date(),
-      }
-
-      let { error } = await supabase.from('profiles').upsert(updates, {
-        returning: 'minimal', // Don't return the value after inserting
-      })
-
-      if (error) {
-        throw error
-      }
-    } catch (error) {
-      alert(error.message)
-    } finally {
-      setLoading(false)
-    }
-  }
 
   return <>
     <h1>Settings</h1>
 
-    <form onSubmit={onSubmit}>
+    <form>
       <label htmlFor="email">Email</label>
       <input
         id="email"
@@ -45,21 +15,9 @@ export default () => {
         value={user && user.email}
         disabled
       />
-
-      <br/>
-
-      <label htmlFor="username">Username</label>
-      <input
-        id="username"
-        type="text"
-        value={username || ''}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-
-      <br/>
-
-      <button type="submit" disabled={loading}>{loading ? 'Updating...' : 'Update'}</button>
     </form>
+
+    <UserName />
 
     <hr />
 
---
+++ src/views/settings/UserName.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+import { supabase } from '@/_util/supabaseClient'
+import { useUser } from '@/_state/user'
+
+export default () => {
+  const { user } = useUser()
+  const [username, setUsername] = useState(user?.username)
+  const [isEditing, setIsEditing] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const onSubmit = async event => {
+    event.preventDefault()
+    try {
+      setLoading(true)
+      const updates = {
+        id: user.id,
+        username,
+        updated_at: new Date(),
+      }
+
+      let { error } = await supabase.from('profiles').upsert(updates, {
+        returning: 'minimal', // Don't return the value after inserting
+      })
+
+      if (error) {
+        throw error
+      }
+    } catch (error) {
+      alert(error.message)
+    } finally {
+      setLoading(false)
+      setIsEditing(false)
+    }
+  }
+
+  return <>
+    <form onSubmit={onSubmit}>
+      <label htmlFor="username">Username</label>
+      {
+        !isEditing ?
+        <>
+          {username || '[None set]'}
+          {' - '}
+          <button onClick={event => {
+            event.preventDefault()
+            setIsEditing(true)
+          }}>
+            edit
+          </button>
+        </>
+        :
+        <>
+          <input
+            id="username"
+            type="text"
+            value={username || ''}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button type="submit" disabled={loading}>{loading ? 'Updating...' : 'Update'}</button>
+        </>
+      }
+    </form>
+  </>
+}
