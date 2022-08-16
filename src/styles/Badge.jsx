c8bf2c2fc098378321280c95a73909bd19469250
Sam Pierce Lolla <sampiercelolla@gmail.com>
2022-08-16 15:16:31 -0400

    homepage copy

---
+++ src/styles/Badge.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components/macro'
+
+export const Badge = styled.span`
+  background: #eee;
+  padding: 2px 5px;
+  font-size: 13px;
+  font-weight: 500;
+  margin: 0 0 0 .5rem;
+`
--- src/views/marketing/Home.jsx
+++ src/views/marketing/Home.jsx
@@ -1,72 +1,101 @@
 import { useNavigate } from 'react-router-dom'
 
 import { Button } from '@/styles/Button'
+import { Badge } from '@/styles/Badge'
 import { OneCol } from '@/styles/Layout'
 import LanguagePicker from '../user_languages/LanguagePicker'
-// import Modal from '@/styles/Modal'
 
 export default () => {
   let navigate = useNavigate()
 
   return <OneCol>
 
-    {/* <Modal close={() => navigate('/languages')}> */}
-      {/* <h1>Learn a foreign language, faster than you ever thought possible</h1> */}
-      <h1>The language site for serious learners</h1>
-      {/* <p>Language learning site for serious language learners.</p> */}
-      <Button onClick={() => navigate('/languages')}>Choose your language</Button>
-      <p>Free forever · open source · no account required</p>
-      {/* Join 10,000 learners broadening their horizons */}
+    <h1>Coming soon: the language site for serious learners</h1>
+    {/* <Button onClick={() => navigate('/languages')}>Choose your language</Button> */}
+    <p>Free forever · open source · no account required</p>
 
-      <hr />
+    <blockquote>
+      "It's like Duolingo for adults" - Joe Flateau
+    </blockquote>
 
-      {/* <blockquote>
-        "Straight to the dome"
-      </blockquote> */}
-      <blockquote>
-        "It's like Duolingo for adults" - Joe Flateau
-      </blockquote>
+    <p>Protolang includes...</p>
+    <ul>
+      <li>
+        <strong>🧑‍🏫 Lessons</strong>
+        {' - '}
+        interactive courses in vocabulary, pronunciation, grammar, and culture
+      </li>
+      <li>
+        <strong>🤔 Flashcards</strong>
+        {' - '}
+        practice your knowledge with simple, interactive audio and text quizzes
+      </li>
+      <li>
+        <strong>💡 Mnemonics</strong>
+        {' - '}
+        tips to help you retain words without rote memorization
+      </li>
+      <li>
+        <strong>📖 Dictionaries</strong>
+        {' - '}
+        built-in word definitions, tenses and conjugations
+      </li>
+      <li>
+        <strong>📈 Analytics</strong>
+        {' - '}
+        track your vocabulary, days practiced, and more
+      </li>
+      <li>
+        <strong>🏰 Stories &amp; prompts</strong>
+        <Badge>Coming soon</Badge>
+        {' - '}
+        read and write about realistic scenarios matched to your skill level
+      </li>
+      <li>
+        <strong>🌍 Resources</strong>
+        <Badge>Coming soon</Badge>
+        {' - '}
+        third party tools and products to round out your knowledge, ranked by our community
+      </li>
+      <li>
+        <strong>👨‍👩‍👧‍👦 Community</strong>
+        <Badge>Coming soon</Badge>
+        {' - '}
+        practice your skills with other language learners from around the world
+      </li>
+    </ul>
 
-      <h2>Why Protolang?</h2>
-      <ul>
-        <li>
-          <strong>No games</strong>
-          <p>We don't tease you with fake prizes or force your down a narrow path. We just make everything available up-front and let you choose the lessons that you need.</p>
-          {/* <p><em>Example: food, workplace, family</em></p> */}
-        </li>
-        {/* <li>
-          <strong>Memorization tricks built-in</strong>
-          <p>Hints from our community help you memorize vocabulary fast</p>
-          <p><em>Example: to remember that caber is Spanish for "to fit inside", imagine a bear fitting inside a taxi cab</em></p>
-        </li> */}
-        <li>
-          <strong>Powerful analytics</strong>
-          <p>To learn faster, we show you where you're succeeding and where you need to focus most.</p>
-          {/* <p><em>Example: vocabulary score over time, daily heatmap, most difficult words</em></p> */}
-        </li>
-      </ul>
+    <p>Why Protolang?</p>
+    <ul>
+      <li>
+        <strong>Freedom to learn your way</strong>
+        {' - '}
+        No fake prizes or narrow learning paths. Choose the lessons and tools that work for you
+      </li>
+      <li>
+        <strong>Focus on listening and speaking</strong>
+        {' - '}
+        Protolang includes reading and writing, but focuses you on audio—the way we all learned our first language
+      </li>
+      <li>
+        <strong>Immersive environment</strong>
+        {' - '}
+        Stories, audio, video, and more: Protolang helps you surround yourself with your new language without ever leaving your seat
+      </li>
+      <li>
+        <strong>Built on open source data</strong>
+        {' - '}
+        Protolang uses vast open-source datasets and the latest browser technology to learning richer than ever before
+      </li>
+    </ul>
 
-      <h2>More features</h2>
-      <ul>
-        <li>In-depth language lessons</li>
-        <li>Customizable flashcards</li>
-        <li>Speech recognition</li>
-        <li>Speech synthesis</li>
-      </ul>
+    {/* <p>
+      Considering a switch?
+      {' '}
+      <a href="">Compare to Duolingo</a>
+    </p> */}
 
-      <p>
-        Protolang is 100% free to use and open source
-      </p>
-
-      {/* <p>
-        Considering a switch?
-        {' '}
-        <a href="">Compare to Duolingo</a>
-      </p> */}
-
-      <LanguagePicker />
-
-    {/* </Modal> */}
+    <LanguagePicker />
 
   </OneCol>
 }
