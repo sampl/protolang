de07e5c114b0b753b273b673b309520bbfc47369
Sam Pierce Lolla <sampiercelolla@gmail.com>
2023-02-03 12:13:23 -0500

    marketing copy update

--- src/views/marketing/Home.jsx
+++ src/views/marketing/Home.jsx
@@ -1,24 +1,24 @@
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 
-import { Button } from '@/styles/Button'
+// import { Button } from '@/styles/Button'
 import { Badge } from '@/styles/Badge'
 import { OneCol } from '@/styles/Layout'
 import LanguagePicker from '../user_languages/LanguagePicker'
 
 export default () => {
-  let navigate = useNavigate()
+  // let navigate = useNavigate()
 
   return <OneCol>
 
     <h1>Coming soon: the language site for serious learners</h1>
     {/* <Button onClick={() => navigate('/languages')}>Choose your language</Button> */}
-    <p>Free forever · open source · no account required</p>
+    <p>Free & open source{/* · no account required */}</p>
 
     <blockquote>
       "It's like Duolingo for adults" - Joe Flateau
     </blockquote>
 
-    <p>Protolang includes...</p>
+    <p>Protolang includes:</p>
     <ul>
       <li>
         <strong>🧑‍🏫 Lessons</strong>
@@ -38,12 +38,23 @@ export default () => {
       <li>
         <strong>📖 Dictionaries</strong>
         {' - '}
-        built-in word definitions, tenses and conjugations
+        instant word definitions, tenses, and conjugations
+      </li>
+      <li>
+        <strong>📺 Media</strong>
+        {' - '}
+        watch and listen: videos, podcasts, radio and more
       </li>
       <li>
         <strong>📈 Analytics</strong>
         {' - '}
         track your vocabulary, days practiced, and more
+      </li>
+      <li>
+        <strong>💬 Chat</strong>
+        <Badge>Coming soon</Badge>
+        {' - '}
+        realistic AI-powered conversations in your target language
       </li>
       <li>
         <strong>🏰 Stories &amp; prompts</strong>
@@ -52,16 +63,16 @@ export default () => {
         read and write about realistic scenarios matched to your skill level
       </li>
       <li>
-        <strong>🌍 Resources</strong>
-        <Badge>Coming soon</Badge>
-        {' - '}
-        third party tools and products to round out your knowledge, ranked by our community
-      </li>
-      <li>
         <strong>👨‍👩‍👧‍👦 Community</strong>
         <Badge>Coming soon</Badge>
         {' - '}
-        practice your skills with other language learners from around the world
+        practice with other language learners from around the world
+      </li>
+      <li>
+        <strong>🌍 Resources</strong>
+        <Badge>Coming soon</Badge>
+        {' - '}
+        other apps to round out your knowledge, ranked by our community
       </li>
     </ul>
 
@@ -70,7 +81,7 @@ export default () => {
       <li>
         <strong>Freedom to learn your way</strong>
         {' - '}
-        No fake prizes or narrow learning paths. Choose the lessons and tools that work for you
+        No fake prizes or narrow learning paths. Choose the lessons and tools that work for you.
       </li>
       <li>
         <strong>Focus on listening and speaking</strong>
@@ -80,12 +91,12 @@ export default () => {
       <li>
         <strong>Immersive environment</strong>
         {' - '}
-        Stories, audio, video, and more: Protolang helps you surround yourself with your new language without ever leaving your seat
+        Protolang surrounds you with your new language without ever leaving your seat
       </li>
       <li>
-        <strong>Built on open source data</strong>
+        <strong>Built on open data</strong>
         {' - '}
-        Protolang uses vast open-source datasets and the latest browser technology to learning richer than ever before
+        Protolang uses vast, publicly-available datasets and the latest browser technology to make learning richer than ever before
       </li>
     </ul>
 
