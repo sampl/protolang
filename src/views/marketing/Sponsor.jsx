6450518e1a6ab1e2ec07d85db1e285fa50fd1989
Sam Pierce Lolla <sampiercelolla@gmail.com>
2023-02-12 14:19:24 -0500

    sponsor typos

--- src/views/marketing/Sponsor.jsx
+++ src/views/marketing/Sponsor.jsx
@@ -6,10 +6,10 @@ export default () => {
     <h1>Support free language education</h1>
 
     <p>
-      Protolang is free to user and <a href="/open-source">open source</a>.
+      Protolang is free to use and <a href="/open-source">open source</a>.
     </p>
     <p>
-      Like every website, we still need money to keep the servers running. If you have the means, please consider supporting this project with a donation.
+      Like every website, we need money to keep the servers running. If you can, please consider supporting this project with a donation.
     </p>
 
     <a href="mailto:sam@directedworks.com">Sponsor this project</a>
