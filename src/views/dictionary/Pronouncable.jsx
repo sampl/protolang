d884058ee22fad6e626741375d73f420e67c57fb
Sam Pierce Lolla <sampiercelolla@gmail.com>
2023-02-23 13:00:09 -0500

    start showing some pronunciation info

---
+++ src/views/dictionary/Pronouncable.jsx
@@ -0,0 +1,30 @@
+import Popover from '@/styles/Popover'
+import styled from 'styled-components/macro'
+
+export default ({ ipa }) => {
+  // debugger
+  if (!ipa) return 'NO IPA'
+  const syllables = ipa?.split('.')
+  if (!syllables || syllables.length < 1) return 'SPLIT ISSUE'
+  return <SyllablesWrapper>
+    {syllables.map(syllable => {
+      return <Popover
+        target={<SyllableWrapper>{syllable || 'NO TEXT'}</SyllableWrapper>}
+        content={<div>
+          <p>IPA: {ipa}</p>
+          {/* <p>Stress: {syllable.stress}</p> */}
+        </div>}
+      />
+    })}
+  </SyllablesWrapper>
+}
+
+export const SyllablesWrapper = styled.span`
+  display: inline-flex;
+  gap: .25rem;
+`
+export const SyllableWrapper = styled.span`
+  cursor: pointer;
+  border-radius: 3px;
+  background: #eee;
+`
--- src/views/practice/AnswerWord.jsx
+++ src/views/practice/AnswerWord.jsx
@@ -5,9 +5,10 @@ import styled from 'styled-components/macro'
 import SpeakWord from '../dictionary/SpeakWord'
 import MnemonicSuggested from '@/views/mnemonics/MnemonicSuggested'
 import { supabaseDictionaries, useSupabaseQuery } from '@/db/supabase'
+import Pronouncable from '../dictionary/Pronouncable'
 
 // TODO
-// see/play pronunciation of each syllable
+// play pronunciation of each syllable
 // For verbs, show conjugation tables
 // more mnemonics?
 
@@ -31,9 +32,19 @@ export default ({ wordString }) => {
       (!definitions || definitions.length === 0) ? <div>(Not in our dictionary yet)</div> :
       <>
         <ul>
-          {definitions.map(word => <li>
-            {word._wiktionary_data?.senses[0].glosses.join('\n')}
-          </li>)}
+          {definitions.map(word => {
+            const sounds = word._wiktionary_data?.sounds
+            const ipaSounds = sounds?.filter(sound => sound.ipa)
+            const ipa = ipaSounds?.length > 0 ? ipaSounds[0].ipa : null
+            const rhymeSounds = sounds?.filter(sound => sound.rhyme)
+            const rhyme = rhymeSounds?.length > 0 ? rhymeSounds[0].rhyme : null
+            return <li key={word.id}>
+              {ipa && <Pronouncable ipa={ipa} />}
+              {' '}
+              {word._wiktionary_data?.senses[0].glosses.join('\n')}
+              {rhyme && <div>rhyme: {rhyme}</div>}
+            </li>
+          })}
         </ul>
         <Link to={`/${langId}/dictionary/${wordString}`}>{definitions.length !== 1 ? 'all definitions' : 'full definition'}</Link>
       </>
