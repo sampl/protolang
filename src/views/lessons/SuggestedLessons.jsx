f18d5d6c050d318b0bf279b9b94de243cfaee7df
Sam Pierce Lolla <sampiercelolla@gmail.com>
2023-02-23 12:43:17 -0500

    only show lessons suggestions in their interests

--- src/views/lessons/SuggestedLessons.jsx
+++ src/views/lessons/SuggestedLessons.jsx
@@ -1,25 +1,25 @@
 import { Link, useParams } from 'react-router-dom'
 import styled from 'styled-components/macro'
-import {groupBy} from 'lodash'
 
 import { supabase, useSupabaseQuery } from '@/db/supabase'
+import { useLanguage } from '@/_state/language'
 
 export default () => {
   const { langId } = useParams()
+  const { currentUserLanguage } = useLanguage()
 
+  // TODO - remove completed lessons
   const query = supabase
     .from('lessons')
-    .select()
+    .select('*, current_edit(*)')
     .eq('language_id', langId)
-    // TODO - sort by order and remove completed lessons
+    .order('sort_order',  { ascending: true })
   const [lessons, loading, error] = useSupabaseQuery(query, [langId])
 
-  // TODO - get user's preferred topics
-  const userTopics = ['animals', 'food']
+  const userTopics = currentUserLanguage?.preferences?.topics || []
 
-  // TODO - filter this on the server
   const suggestedLessons = lessons?.filter(l => {
-    return l.topics?.some(t => userTopics.includes(t))
+    return l.current_edit?.topics?.some(t => userTopics.includes(t))
   })
 
   if (error) return error.message
@@ -27,7 +27,7 @@ export default () => {
   if (suggestedLessons.length < 1) return '' // just don't show anything
 
   return <>
-    <p>Suggested lesson{suggestedLessons.length !== 1 && 's'}</p>
+    <h2>Based on your interests</h2>
     <SuggestedLessonsWrapper>
       {suggestedLessons.map(lesson => {
         return <SuggestedLessonWrapper key={lesson.slug} to={`/${langId}/lessons/${lesson.slug}`}>
@@ -35,19 +35,20 @@ export default () => {
         </SuggestedLessonWrapper>
       })}
     </SuggestedLessonsWrapper>
-    <hr />
+    <br />
   </>
 }
 
 const SuggestedLessonsWrapper = styled.div`
-  display: flex;
+  /* display: flex;
   flex-direction: row;
-  gap: 2rem 1rem;
-  overflow-x: auto;
+  gap: 1rem 0.5rem;
+  flex-wrap: wrap; */
 `
 const SuggestedLessonWrapper = styled(Link)`
-  border: 1px solid;
+  /* border: 1px solid;
   padding: 1rem;
   text-decoration: none;
-  min-height: 100px;
+  min-height: 60px;
+  min-width: 200px; */
 `
