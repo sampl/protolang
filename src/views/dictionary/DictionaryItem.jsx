3431ee021b205720c605b1eb849c18468e0a2f3f
Sam Pierce Lolla <sampiercelolla@gmail.com>
2023-02-23 11:42:58 -0500

    handle dictionary words with multiple entries

--- src/views/dictionary/DictionaryItem.jsx
+++ src/views/dictionary/DictionaryItem.jsx
@@ -15,85 +15,88 @@ export default () => {
     .from(langId)
     .select()
     .eq('word', wordName)
-    .single()
-  const [word, loading, error] = useSupabaseQuery(query)
-
-  if (word) {
-    var {
-      pos,
-      categories,
-      senses,
-      forms,
-      etymology_text,
-      sounds,
-    } = word?._wiktionary_data
-    var ipa = sounds?.[0]?.ipa
-  }
+  const [definitions, loading, error] = useSupabaseQuery(query)
 
   // TODO - calculate your accuracy and word score here
   return <div>
-    {
-      error ? error.message :
-      loading ? 'loading...' :
-      !word ? 'Word not found' :
-      <>
-        <h1>
-          {word?.word}
-          <SpeakWord wordString={word?.word} />
-        </h1>
-        <TwoColumns>
-          <div>
-            <p>Part of speech: {pos}</p>
-            <p>Categories: {categories?.join(', ')}</p>
-            {etymology_text && <p>{etymology_text}</p>}
-            {ipa && <p>IPA: {ipa}</p>}
-            <hr />
+    <h1>
+      {wordName}
+      <SpeakWord wordString={wordName} />
+    </h1>
+    <TwoColumns cols="5fr 3fr">
+      <div>
+        {
+          error ? error.message :
+          loading ? 'loading...' :
+          (!definitions || definitions.length <= 0) ? 'Definitions not found' :
+          <>
+            {definitions.map(word => {
 
-            <h2>Forms</h2>
-            {forms?.map( f => {
-              return <span key={f.form}>
-                <Link to={`/${langId}/dictionary/${f.form}`}>{f.form}</Link>
-                {' '}
-              </span>
-            })}
+            var {
+              pos,
+              categories,
+              senses,
+              forms,
+              etymology_text,
+              sounds,
+            } = word._wiktionary_data
+            var ipa = sounds?.[0]?.ipa
 
-            <h2>Senses</h2>
-            {senses?.map( sense => {
-              return <div key={sense.id}>
-                <p>{sense?.tags?.join(', ')}</p>
+            return <div>
+                {word._wiktionary_data?.senses[0].glosses.join('\n')}
+
+                <p>Part of speech: {pos}</p>
+                <p>Categories: {categories?.join(', ')}</p>
+                {etymology_text && <p>{etymology_text}</p>}
+                {ipa && <p>IPA: {ipa}</p>}
+                <hr />
+
+                <h2>Forms</h2>
+                {forms?.map( f => {
+                  return <span key={f.form}>
+                    <Link to={`/${langId}/dictionary/${f.form}`}>{f.form}</Link>
+                    {' '}
+                  </span>
+                })}
+
+                <h2>Senses</h2>
+                {senses?.map( sense => {
+                  return <div key={sense.id}>
+                    <p>{sense?.tags?.join(', ')}</p>
+                  </div>
+                })}
               </div>
             })}
+          </>
+        }
+      </div>
+      <div>
+        <a
+          href={`https://translate.google.com/?sl=${langId}&tl=en&text=${wordName}&op=translate`}
+          target="_blank"
+        >
+          Open in Google Translate
+        </a>
+        {' · '}
+        <a
+          href={`https://en.wiktionary.org/wiki/${wordName}#Italian`}
+          target="_blank"
+        >
+          Open in Wiktionary
+        </a>
+        {' · '}
+        <a
+          href={`https://forvo.com/word/${wordName}/#it`}
+          target="_blank"
+        >
+          Pronounce in Forvo
+        </a>
+        <hr />
+        <MnemonicsList string={wordName} />
+        <hr />
+        <Ngram wordString={wordName} />
+      </div>
 
-          </div>
-          <div>
-            <a
-              href={`https://translate.google.com/?sl=${langId}&tl=en&text=${word.word}&op=translate`}
-              target="_blank"
-            >
-              Open in Google Translate
-            </a>
-            {' · '}
-            <a
-              href={`https://en.wiktionary.org/wiki/${word.word}#Italian`}
-              target="_blank"
-            >
-              Open in Wiktionary
-            </a>
-            {' · '}
-            <a
-              href={`https://forvo.com/word/${word.word}/#it`}
-              target="_blank"
-            >
-              Pronounce in Forvo
-            </a>
-            <hr />
-            <MnemonicsList string={word.word} />
-            <hr />
-            <Ngram wordString={word.word} />
-          </div>
-
-        </TwoColumns>
-      </>
-    }
+    </TwoColumns>
   </div>
 }
