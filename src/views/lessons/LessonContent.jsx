c90a9b653ea07889b0bca61316b8156aeadb8846
Sam Pierce Lolla <sampiercelolla@gmail.com>
2023-02-24 09:21:56 -0500

    debounce lesson content changes
    - so we don't slam the server or browser when users edit

--- src/views/lessons/LessonContent.jsx
+++ src/views/lessons/LessonContent.jsx
@@ -2,13 +2,16 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import remarkDirective from 'remark-directive'
 import remarkDirectiveRehype from 'remark-directive-rehype'
+import { useDebounce } from 'use-debounce'
+
 import LessonEmbed from './LessonEmbed'
 import LessonVideo from './LessonVideo'
 
 export default ({ content }) => {
+  let [debouncedContent] = useDebounce(content, 1000, { leading: true })
   return <>
     <ReactMarkdown
-      children={content}
+      children={debouncedContent}
       remarkPlugins={[remarkGfm, remarkDirective, remarkDirectiveRehype]}
       components={{
         'word': LessonEmbed, // rename to phrase?
