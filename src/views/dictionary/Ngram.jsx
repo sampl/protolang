39d6ee132d8a21269356ea3c408db6e719d1530a
Sam Pierce Lolla <sampiercelolla@gmail.com>
2023-03-02 13:20:06 -0500

    ngram styles

--- src/views/dictionary/Ngram.jsx
+++ src/views/dictionary/Ngram.jsx
@@ -9,7 +9,18 @@ export default ({ wordString }) => {
 
   return <>
     <h3>Word frequency</h3>
-    <p>
+
+    {/* https://ell.meta.stackexchange.com/questions/4698/how-can-i-insert-a-google-ngram-chart-relatively-easily */}
+    <iframe
+      src={`https://books.google.com/ngrams/interactive_chart?content=${wordUrl}&corpus=${corpusLangCode}&year_start=1800`}
+      width="400"
+      height="240"
+      hspace="0"
+      vspace="0"
+      style={{border: 0, maxWidth: '100%'}}
+    />
+
+    <p style={{fontSize: 'small'}}>
       More about
       {' '}
       <a
@@ -19,15 +30,6 @@ export default ({ wordString }) => {
         Google Ngram
       </a>
     </p>
-
-    {/* https://ell.meta.stackexchange.com/questions/4698/how-can-i-insert-a-google-ngram-chart-relatively-easily */}
-    <iframe
-      src={`https://books.google.com/ngrams/interactive_chart?content=${wordUrl}&corpus=${corpusLangCode}&year_start=1800`}
-      width="400"
-      height="240"
-      hspace="0"
-      vspace="0"
-    />
 
   </>
 }
