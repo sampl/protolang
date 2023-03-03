8357db721f8bf0c2afe9ca09f334dc05a3a55ab7
Sam Pierce Lolla <sampiercelolla@gmail.com>
2023-03-03 18:09:13 -0500

    add attribution section to open source page

--- src/views/marketing/OpenSource.jsx
+++ src/views/marketing/OpenSource.jsx
@@ -8,7 +8,7 @@ export default () => {
     <h1>Open source</h1>
     <p>This project would not be possible without the open-source, community-driven projects it is built on. We are proud to offer our own work under free software licenses.</p>
 
-    <h2>Source code</h2>
+    <h2>Application source code</h2>
     <p>The Protolang web app is available under the GPL.</p>
     <ul>
       <li>
@@ -21,12 +21,20 @@ export default () => {
 
     <h2>Data downloads</h2>
     <p>User-generated content is available to download here. All of this content is free to use under a Creative Commons license (coming soon).</p>
+    <ul>
+      <li>
+        <LessonsDownload />
+      </li>
+      <li>
+        <PhrasesDownload />
+      </li>
+      <li>
+        <MnemonicsDownload />
+      </li>
+    </ul>
 
-    <LessonsDownload />
-    {' '}
-    <PhrasesDownload />
-    {' '}
-    <MnemonicsDownload />
+    <h2>Attribution</h2>
+    <p>Protolang depends on <a href="https://github.com/sampl/protolang/network/dependencies" target="_blank">this open source software</a>.</p>
 
   </OneCol>
 }
